58a102a77daa3e0b3a71f55db0c0a31a5cb4d4da
carlosmelo1295 <carlosmelo1295@gmail.com>
2024-01-20 09:24:54 -0300

    Initialize project and implement product router

---
+++ annotation.md
@@ -0,0 +1,3 @@
+# pip3 install -r requirements.txt
+
+- permite escrever em um bloco de notas as dependências e versões do seu projeto, e instalar através do comando acima
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request  # do flask importe a classe Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'  # configuração do banco
+
+db = SQLAlchemy(app)  # instancia do banco
+
+
+# apos criar a classe, rodar o comando "flask shell" e criar a tabela com o comando "db.create_all()" e "db.session.commit()" para finalizar o commit
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Successfully"}), 200
+    return jsonify({"message": "Invalid product data"}), 400
+
+
+@app.route('/')
+def hello_word():
+    return 'Hello World'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
