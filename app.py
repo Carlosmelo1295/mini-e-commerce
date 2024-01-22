5d9dcf2a3e4ecabf029c6930ea28c494c0527e88
carlosmelo1295 <carlosmelo1295@gmail.com>
2024-01-21 22:46:24 -0300

    authenticate and protect router

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'minha_chave'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'  # configuração do banco
+login_manager = LoginManager()
 
 db = SQLAlchemy(app)  # instancia do banco
+CORS(app)
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'  # Login manager faz o gerenciamento dos usuários para sabermos quem está logado
+
 
 # apos criar a classe, rodar o comando "flask shell" e criar a tabela com o comando "db.create_all()" e "db.session.commit()" para finalizar o commit
 class Product(db.Model):
@@ -13,32 +22,38 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-# ao criar outra tabela, rodar 
+
+# ao criar outra tabela, rodar
 # >>> db.drop_all()
 # >>> db.create_all()
 # >>> db.session.commit()
 # >>> exit()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
 
+#usado para recuperar o usuário sempre que tentar acessar uma rota protegida
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))#user id recuperado dos cokies
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     user = User.query.filter_by(username=data.get("username")).first()
-    
+
     if user and data.get("password") == user.password:
+        login_user(user)
         return jsonify({"message": "Login Successfully"})
-    
+
     return jsonify({"message": "Access denied"}), 401
 
 
-    
 @app.route('/api/products/add', methods=["POST"])
+@login_required # diz para a rota que só pode ser acessada por usuários
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -48,7 +63,9 @@ def add_product():
         return jsonify({"message": "Successfully"})
     return jsonify({"message": "Invalid product data"}), 400
 
+
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -56,6 +73,7 @@ def delete_product(product_id):
         db.session.commit()
         return jsonify({"message": "Product deleted Successfully"})
     return jsonify({"message": "Product not found"}), 404
+
 
 @app.route('/api/products/<int:product_id>', methods=["GET"])
 def get_product_details(product_id):
@@ -65,16 +83,19 @@ def get_product_details(product_id):
             "id": product.id,
             "name": product.name,
             "price": product.price,
-            "description": product.description,  
+            "description": product.description,
         })
     return jsonify({"message": "Product not found"}), 404
 
+
 @app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
+
 def update_products(product_id):
     product = Product.query.get(product_id)
     if not product:
         return jsonify({"message": "Product not found"}), 404
-    
+
     data = request.json
     if 'name' in data or 'price' in data or 'description' in data:
         product.name = data['name']
@@ -83,13 +104,14 @@ def update_products(product_id):
         db.session.commit()
     return jsonify({"message": "Product updated Successfully"})
 
+
 @app.route('/api/products', methods=['GET'])
 def get_products():
     products = Product.query.all()
-    
+
     if not products:
         return jsonify({'message': 'No product found'}), 404
-        
+
     list_of_products = []
     for product in products:
         product_data = {
@@ -98,9 +120,10 @@ def get_products():
             "price": product.price,
         }
         list_of_products.append(product_data)
-    
+
     return list_of_products
-    
+
+
 @app.route('/')
 def hello_word():
     return 'Hello World'
